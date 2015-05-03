ee2bab890951c78784771d9414083b149af41992
CharlesHoang <charles.b.hoang@gmail.com>
2015-05-03 16:00:11 -0400

    Add client write functionality

--- simpleChatServer.js
+++ simpleChatServer.js
@@ -1,11 +1,38 @@
 var net = require("net");
 
-var chatServer = net.createServer();
+var chatServer = net.createServer(),
+clientList = [];
 
 chatServer.on("connection", function(client){
-	client.write("Hi!\n");
-	client.write("Bye!\n");
-	client.end();
+	client.name = client.remoteAddress + ":" + client.remotePort;
+	client.write("Hi! " + client.name + '!\n');
+	
+	clientList.push(client);
+	
+	client.on("data", function(data){
+		broadcast(data,client);
+	});
+	
+	client.on("end", function(){
+		clientList.splice(clientList.indexOf(client), 1);
+	});
 });
+
+function broadcast(message,client){
+	var cleanup = [];
+	for (var i = 0; i < clientList.length; i++){
+		if (client !== clientList[i]){
+			if (clientList[i].writable){
+				clientList[i].write(client.name + " says " + message);
+			}else{
+				cleanup.push(clientList[i]);
+				clientList[i].destroy();
+			}
+		}
+	}
+	//Remove dead nodes out of write loop to avoid interfering with loop index
+	for (i = 0; i < cleanup.length; i ++)
+		clientList[i].splice(clientList[i].indexOf(cleanup[i]),1);
+}
 
 chatServer.listen(9000);
